29d9205e99e703edad5a3f667b68ae4034d0c1cc
Ross <ross@duris.io>
2022-10-26 19:04:46 -0400

    init commit

---
+++ components/Canvas.js
@@ -0,0 +1,22 @@
+const Canvas = (
+  {
+    width,
+    height
+  }
+) => {
+  return(
+    <canvas 
+      width={width} 
+      height={height} 
+      style={canvasStyle}
+      
+    />
+  )
+}
+
+export default Canvas
+
+const canvasStyle  = {
+  border: '1px solid black',
+  background: 'white'
+}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Canvas from "../components/Canvas";
+
+const Index = () => {
+  return(
+    <main className=" h-screen flex bg-slate-900">
+      <div className=" m-auto">
+      <Canvas
+        width={600}
+        height={600}
+       />
+       </div>
+    </main>
+  )
+}
+
+export default Index
